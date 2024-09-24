befdf242f96424991b78604744e708485ceaead4
JJ <yurk.jur@gmail.com>
2024-09-24 21:08:48 +0200

    issue host fixed

--- src/main/java/com/vitte/notification/service/notification/EmailNotificator.java
+++ src/main/java/com/vitte/notification/service/notification/EmailNotificator.java
@@ -28,13 +28,7 @@ public class EmailNotificator implements Notificator {
     public boolean sendNotification(ContactDto contact, NotificationEntity savedNotification) {
         try {
             if (contact.isEnabled()) {
-                String host = "http://localhost:8080";
-                String link = host + savedNotification.getLinkToConversation();
-                SimpleMailMessage message = new SimpleMailMessage();
-                message.setTo(contact.getContactInfo());
-                message.setFrom("Служба поддержки <ask.me.kontakt@gmail.com>");
-                message.setSubject("У вас новый комментарий");
-                message.setText("На вашу заявку был дан новый комментарий. Что увидеть подробности перейдите по ссылке: " + link);
+                SimpleMailMessage message = getSimpleMailMessage(contact, savedNotification);
                 mailSender.send(message);
 
                 log.info("{}-notification sent successfully!", TYPE);
@@ -48,6 +42,17 @@ public class EmailNotificator implements Notificator {
             log.error("{}-Notification was not send", TYPE, e);
             return false;
         }
+    }
+
+    private static SimpleMailMessage getSimpleMailMessage(ContactDto contact, NotificationEntity savedNotification) {
+        String host = "https://vitte-core.up.railway.app";
+        String link = host + savedNotification.getLinkToConversation();
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(contact.getContactInfo());
+        message.setFrom("Служба поддержки <ask.me.kontakt@gmail.com>");
+        message.setSubject("У вас новый комментарий");
+        message.setText("На вашу заявку был дан новый комментарий. Что увидеть подробности перейдите по ссылке: " + link);
+        return message;
     }
 
 }
